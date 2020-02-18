8a538b873b051c975f61b18ee300d75f75a5abd3
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-02-18 12:26:27 +0900

    -

--- val.py
+++ val.py
@@ -3,10 +3,11 @@
 import sys, os, re, argparse
 sys.path.insert(0, os.getcwd())
 from cbilib.catalog import get_catalog
+from math import log10
 
 def main(args):
-    pat_Kd = re.compile('([-0-9.]+)(mM|uM|nM|pM)')
-    factor = {'mM': 1000000, 'uM': 1000, 'nM': 1, 'pM': 0.001}
+    pat_Kd = re.compile('([-0-9.]+)(mM|uM|nM|pM|fM)')
+    factor = {'mM': 1e-3, 'uM': 1e-6, 'nM': 1e-9, 'pM': 1e-12, 'fM': 1e-15}
 
     csv_name = args.csv
     odir = args.out
@@ -16,10 +17,10 @@ def main(args):
     for i in df.index:
         pdbid, Kd = df.loc[i, ['PDB_code', 'Kd']]
         m = pat_Kd.match(Kd)
-        D[pdbid] = float(m.group(1))*factor[m.group(2)]
+        D[pdbid] = -log10(float(m.group(1))*factor[m.group(2)])
     for pdbid in D:
         if os.path.exists(f'{odir}/{pdbid}'):
-            v = D[pdbid]
+            v = round(D[pdbid], 3)
             print(pdbid, v)
             print(v, file=open(f'{odir}/{pdbid}/value', 'wt'))
 
