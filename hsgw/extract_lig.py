f291dfc74d0797f08524bafb46d4eda387c77161
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-02-19 19:42:10 +0900

    -

--- hsgw/extract_lig.py
+++ hsgw/extract_lig.py
@@ -1,5 +1,7 @@
+from rdkit import Chem
 import argparse
 import gzip
+import numpy as np
 
 """
 0         1         2         3         4         5         6
@@ -12,6 +14,7 @@ def main(args):
     oname = f'{ligand_name}.pdb'
     openf = gzip.open if pdb_name.endswith('.gz') else open
 
+    atomnames = set()
     ligands = {}
     for line in openf(pdb_name, 'rt'):
         line = line.rstrip()
@@ -19,18 +22,44 @@ def main(args):
             pdb_tag = line[:6]
             pdb_ligname = line[17:20]
             pdb_chainId = line[21]
+            pdb_atomname = line[13:16].strip()
         except:
             continue
         if pdb_tag == 'HETATM' and pdb_ligname == ligand_name:
+            if pdb_atomname in atomnames:
+                continue
             if pdb_chainId not in ligands:
                 ligands[pdb_chainId] = []
             ligands[pdb_chainId].append(line)
+            atomnames.add(pdb_atomname)
 
     chainIds = sorted(ligands)
     chainId = chainIds[0]
+    mol = Chem.MolFromPDBBlock('\n'.join(ligands[chainId]))
+    print(Chem.MolToSmiles(mol), ligand_name)
+    mols = list(Chem.GetMolFrags(mol, asMols=True))
+    if 1 < len(mols):
+        mols.sort(key=lambda m: m.GetNumAtoms(), reverse=True)
+    mol = mols[0]
+    lines = Chem.MolToPDBBlock(mol).split('\n')
+    #ref_xyzs = np.array([(float(line[30:38]), float(line[38:46]), float(line[46:54])) for line in lines if line.startswith('HETATM')])
+    ##print(ref_xyzs)
+    ##print('\n'.join(lines))
     with open(oname, 'wt') as out:
-        for line in ligands[chainId]:
-            out.write(line + '\n')
+        #for line in ligands[chainId]:
+        #        xyz = np.array([float(line[30:38]), float(line[38:46]), float(line[46:54])])
+        #        found = False
+        #        for i in range(ref_xyzs.shape[0]):
+        #            v = xyz - ref_xyzs[i]
+        #            d2 = np.sum(v*v)
+        #            if d2 < 0.1:
+        #                found = True
+        #                break
+        #        if found:
+        #            out.write(line + '\n')
+        for line in lines:
+            if line.startswith('HETATM'):
+                out.write(line + '\n')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
--- hsgw/rmsd.py
+++ hsgw/rmsd.py
@@ -1,89 +1,24 @@
-import argparse
+import sys, argparse, math
 from rdkit import Chem
-from rdkit.Chem import AllChem
 import numpy as np
-import math
 
-"""
-0         1         2         3         4         5         6         7
-01234567890123456789012345678901234567890123456789012345678901234567890123456789
-HETATM10147  C7  PNM A1007      25.104  27.308  86.289  1.00 42.92           C
-"""
-
-ATOMS = dict(H=1, C=6, N=7, O=8, F=9, P=15, S=16, CL=17, BR=35, I=53)
-
-def mk_mol_atom_only(atoms):
-    mol = Chem.RWMol()
-    conf = Chem.Conformer(len(atoms))
-    for i, (el, x, y, z) in enumerate(atoms):
-        atom = Chem.Atom(el)
-        mol.AddAtom(atom)
-        conf.SetAtomPosition(i, [x, y, z])
-    mol.AddConformer(conf)
-    return Chem.Mol(mol)
+def read_mol2(fname):
+    mol = Chem.MolFromMol2File(fname, sanitize=False)
+    Chem.SanitizeMol(mol, Chem.SANITIZE_CLEANUP | Chem.SANITIZE_CLEANUPCHIRALITY)
+    mol = Chem.RemoveHs(mol)
+    return mol
 
 def read_pdb(fname):
-    atoms = []
-    for line in open(fname, 'rt'):
-        line = line.strip()
-        if not line.startswith('HETATM'):
-            continue
-        el = line[76:78].strip().upper()
-        if el not in ATOMS:
-            raise Exception(f'{el} not in ATOMS')
-        if el == 'H':
-            continue
-        x = float(line[30:38])
-        y = float(line[38:46])
-        z = float(line[46:54])
-        atoms.append((ATOMS[el], x, y, z))
-    return mk_mol_atom_only(atoms)
+    mol = Chem.MolFromPDBFile(fname, sanitize=False)
+    Chem.SanitizeMol(mol, Chem.SANITIZE_CLEANUP | Chem.SANITIZE_CLEANUPCHIRALITY)
+    mol = Chem.RemoveHs(mol)
+    return mol
 
 def read_sdf(fname):
-    f = open(fname, 'rt')
-    try:
-        while True:
-            next(f)
-            next(f)
-            next(f)
-            countline = next(f)
-            it = countline.split()
-            natoms = int(it[0])
-            atoms = []
-            for i in range(natoms):
-                it = next(f).strip().split()
-                x, y, z, el = float(it[0]), float(it[1]), float(it[2]), it[3].upper()
-                if el not in ATOMS:
-                    raise Exception(f'{el} not in ATOMS')
-                if el == 'H':
-                    continue
-                atoms.append((ATOMS[el], x, y, z))
-            yield mk_mol_atom_only(atoms)
-            while True:
-                line = next(f).strip()
-                if line == '$$$$':
-                    break
-    except StopIteration:
-        pass
-
-def read_sdf_first_mol(fname):
-    f = open(fname, 'rt')
-    next(f)
-    next(f)
-    next(f)
-    countline = next(f)
-    it = countline.split()
-    natoms = int(it[0])
-    atoms = []
-    for i in range(natoms):
-        it = next(f).strip().split()
-        x, y, z, el = float(it[0]), float(it[1]), float(it[2]), it[3].upper()
-        if el not in ATOMS:
-            raise Exception(f'{el} not in ATOMS')
-        if el == 'H':
-            continue
-        atoms.append((ATOMS[el], x, y, z))
-    return mk_mol_atom_only(atoms)
+    for mol in Chem.SDMolSupplier(fname, sanitize=False):
+        Chem.SanitizeMol(mol, Chem.SANITIZE_CLEANUP | Chem.SANITIZE_CLEANUPCHIRALITY)
+        mol = Chem.RemoveHs(mol)
+        yield mol
 
 def print_mol(mol):
     for atom in mol.GetAtoms():
@@ -91,7 +26,11 @@ def print_mol(mol):
     print()
 
 def get_rmsd(mol1, mol2):
-    if Chem.MolToSmiles(mol1) != Chem.MolToSmiles(mol2):
+    smi1 = Chem.MolToSmiles(mol1)
+    smi2 = Chem.MolToSmiles(mol2)
+    if smi1 != smi2:
+        print(smi1, 'ref', file=sys.stderr)
+        print(smi2, 'docked did not match', file=sys.stderr)
         return -1.0
     order1 = [x[1] for x in sorted(list(zip(Chem.CanonicalRankAtoms(mol1), range(mol1.GetNumAtoms()))))]
     order2 = [x[1] for x in sorted(list(zip(Chem.CanonicalRankAtoms(mol2), range(mol2.GetNumAtoms()))))]
@@ -106,54 +45,15 @@ def get_rmsd(mol1, mol2):
     rmsd = math.sqrt(s/mol1.GetNumAtoms())
     return rmsd
 
-def presanitize(mol):
-    for atom in mol.GetAtoms():
-        n = atom.GetAtomicNum()
-        deg = atom.GetDegree()
-        if n == 6:
-            if 4 < deg:
-                atom.SetFormalCharge(deg-4)
-        elif n == 7:
-            if 3 < deg:
-                atom.SetFormalCharge(deg-3)
-        elif n == 8:
-            if 2 < deg:
-                atom.SetFormalCharge(deg-2)
-
-def connect_nearest(mol):
-    rwmol = Chem.RWMol(mol)
-    conf = rwmol.GetConformer(0)
-    coords = np.array(([list(conf.GetAtomPosition(i)) for i in range(rwmol.GetNumAtoms())]))
-    thres = 1.9
-    thres2 = thres*thres
-    connected = set()
-    for i in range(coords.shape[0]):
-        r = coords[i]
-        for j in range(coords.shape[0]):
-            if j == i:
-                continue
-            c = coords[j]
-            v = np.sum((c - r)*(c - r))
-            if v < thres2:
-                if (i, j) in connected:
-                    continue
-                rwmol.AddBond(i, j, Chem.BondType.SINGLE)
-                connected.add((i, j))
-                connected.add((j, i))
-    mol = Chem.Mol(rwmol)
-    presanitize(mol)
-    Chem.SanitizeMol(mol)
-    return mol
-
 def main(args):
     ref_name = args.ref_name
     fit_name = args.fit_name
-    mol1 = read_pdb(ref_name)
-    mol1 = connect_nearest(mol1)
+    for mol1 in read_sdf(ref_name):
+        break
+    #mol1 = read_pdb(ref_name)
     count = 0
     for mol2 in read_sdf(fit_name):
         count += 1
-        mol2 = connect_nearest(mol2)
         rmsd = get_rmsd(mol1, mol2)
         rmsd = round(rmsd, 3)
         print(count, rmsd)
