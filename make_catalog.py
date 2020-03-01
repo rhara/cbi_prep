8e5b5aace71458246d038c0f53cb6795161825d6
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-03-01 21:55:48 +0900

    -

--- make_catalog.py
+++ make_catalog.py
@@ -7,7 +7,11 @@ def get_kinase_pdbids():
     df = pd.read_csv('KLIFS_export.csv')
     return set(df['PDB'])
 
-def get_index_data(iname, kinases, src):
+def get_hsgw_selections():
+    df = pd.read_csv('pdb2018_kd_select.csv')
+    return set(df['PDB_code'])
+
+def get_index_data(iname, kinases, selects, src):
     AMINO_ACIDS = 'ALA ARG ASN ASP CYS GLN GLU GLY HIS ILE LEU LYS MET PHE PRO SER THR TRP TYR VAL'.split()
     UNIT = {'M': 1, 'mM': 1e-3, 'uM': 1e-6, 'nM': 1e-9, 'pM': 1e-12, 'fM': 1e-15}
     value_pat = re.compile('^(IC50|Ki|Kd)(=|>|>=|<|<=|~)([.0-9]+)(M|mM|uM|nM|pM|fM)$')
@@ -35,14 +39,20 @@ def get_index_data(iname, kinases, src):
         unit = m.group(4)
         value *= UNIT[unit]
         pvalue = round(-math.log10(value), 3)
-        label = 'kinase' if pdbid in kinases else ''
+        labels = []
+        if pdbid in selects:
+            labels.append('select')
+        if pdbid in kinases:
+            labels.append('kinase')
+        label = ':'.join(labels)
         data[pdbid] = Data(pdbid, ligname, measure, pvalue, src, label)
     return data
 
 def main():
     kinases = get_kinase_pdbids()
-    data_2018 = get_index_data('INDEX_general_PL_data.2018', kinases, '2018')
-    data_2019 = get_index_data('INDEX_general_PL_data.2019', kinases, '2019')
+    selects = get_hsgw_selections()
+    data_2018 = get_index_data('INDEX_general_PL_data.2018', kinases, selects, '2018')
+    data_2019 = get_index_data('INDEX_general_PL_data.2019', kinases, selects, '2019')
     keys_2018 = set(data_2018.keys())
     keys_2019 = set(data_2019.keys())
     data_all = []
